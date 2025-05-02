b2285a899a83cd8bea87cc81eafb95c1410e8536
Aiden1841 <dzc-650-mjc@e-net.nara.jp>
2025-05-02 05:00:29 +0000

    5.C

---
+++ 5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,score;
+printf("点数を入力してください:");
+scanf ("%")  
+    if (a)
+   {
+    /* code */
+   }
+   
+   
+    // scoreが80以上なら「合格」と表示しよう
+    return 0;
+}
+
